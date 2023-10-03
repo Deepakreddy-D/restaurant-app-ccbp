feed452c35b6b48580cfcd6eba06d6105e66c411
jeyaseelan1998 <jeyaseelan.work@gmail.com>
2023-10-03 10:23:49 +0000

    49 test cases passed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,71 @@
+import {useState} from 'react'
+import {Switch, Route} from 'react-router-dom'
+
 import Home from './components/Home'
+import Login from './components/Login'
+import Cart from './components/Cart'
+
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
+
 import './App.css'
 
 // write your code here
-const App = () => <Home />
+const App = () => {
+  const [cartList, setCartList] = useState([])
+  const addItemToCart = dish => {
+    const isAlreadyExists = cartList.find(item => item.dishId === dish.dishId)
+    if (!isAlreadyExists) {
+      const newDish = {...dish, quantity: 1}
+      setCartList(prev => [...prev, newDish])
+    } else {
+      setCartList(prev =>
+        prev.map(item =>
+          item.dishId === dish.dishId
+            ? {...item, quantity: item.quantity + 1}
+            : item,
+        ),
+      )
+    }
+  }
+
+  const removeItemFromCart = dish => {
+    const isAlreadyExists = cartList.find(item => item.dishId === dish.dishId)
+    if (isAlreadyExists) {
+      setCartList(prev =>
+        prev
+          .map(item =>
+            item.dishId === dish.dishId
+              ? {...item, quantity: item.quantity - 1}
+              : item,
+          )
+          .filter(item => item.quantity > 0),
+      )
+    }
+  }
+
+  const removeAllCartItems = () => setCartList([])
+
+  const incrementCartItemQuantity = () => {}
+
+  const decrementCartItemQuantity = () => {}
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartList,
+        incrementCartItemQuantity,
+        decrementCartItemQuantity,
+        removeAllCartItems,
+      }}
+    >
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/cart" component={Cart} />
+      </Switch>
+    </CartContext.Provider>
+  )
+}
 
 export default App
