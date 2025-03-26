2d5b6a967da7eb183be4f40332442f8aa080e4de
Deepak Reddy D <114271318+Deepakreddy-D@users.noreply.github.com>
2025-03-26 23:06:33 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -34,7 +34,7 @@ const Home = () => {
     }))
 
   const fetchRestaurantApi = async () => {
-    const api = 'https://run.mocky.io/v3/72562bef-1d10-4cf5-bd26-8b0c53460a8e'
+    const api = 'https://apis2.ccbp.in/restaurant-app/restaurant-menu-list-details'
     const apiResponse = await fetch(api)
     const data = await apiResponse.json()
     const updatedData = getUpdatedData(data[0].table_menu_list)
